06d498cec58aaa1382d0ea96b9943ef298254a51
Konrad Pason <konrad.pason17@gmail.com>
2017-12-21 12:04:21 +0100

    Implemented pagination for listing categories and styled categories layout

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,7 @@
 class CategoriesController < ApplicationController
 
   def index
-    @category = Category.all
+    @categories = Category.paginate(page: params[:page], per_page: 5)
   end
 
   def new
